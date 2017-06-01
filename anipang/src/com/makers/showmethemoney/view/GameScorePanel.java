030215256b0f2c21885795eeafae8553520edb13
leeshinil <lexshinil@naver.com>
2017-06-01 12:18:57 +0900

    push again

---
+++ anipang/src/com/makers/showmethemoney/view/GameScorePanel.java
@@ -0,0 +1,18 @@
+package com.makers.showmethemoney.view;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class GameScorePanel extends JPanel {
+
+	// 점수 패널 생성 및 설정
+	public GameScorePanel() {
+		this.setBounds(625, 110, 200, 605); // 위치 및 크기조절
+		this.setBackground(Color.CYAN);
+
+	}
+
+}
